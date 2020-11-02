d80a1374e22a7d3419df1579854dd13d55ae3f99
niruhsa <niruhsa@hentai.gg>
2020-11-03 02:06:41 +1100

    Initial Commit

---
+++ src/face_ripper.py
@@ -0,0 +1,129 @@
+import argparse, sys, os, ffmpeg, queue, asyncio, json, face_recognition, cv2, time, numpy as np, pickle
+from multiprocessing import Manager
+from worker import FaceRipperWorker
+
+class FaceRipper:
+
+	def __init__(self, **kwargs):
+		self.kwargs = kwargs
+
+		self.config = self.kwargs['config']
+		self.videos = self.kwargs['video_dir']
+		self.target_dir = self.kwargs['target_dir']
+		self.tolerance = self.kwargs['tolerance']
+		self.workers = self.kwargs['threads'] if self.kwargs['threads'] else 4
+		self.model = self.kwargs['model'] if self.kwargs['model'] else "hog"
+		self.jitters = self.kwargs['jitters']
+		self.blur_threshold = self.kwargs['blur']
+		self.faces = []
+		self.encodings = []
+		self.queue = []
+		self.threads = []
+
+		if self.model == "cnn": print('[WARNING] CUDA support is unstable!')
+		else: os.environ['CUDA_LAUNCH_BLOCKING'] = str(1)
+
+		if self.workers <= 0: self.workers = 1
+
+		self.manager = Manager()
+		self.shared_list = self.manager.list()
+
+		self.loop = asyncio.get_event_loop()
+
+		if not self.config or not self.videos or not self.target_dir: os.system("python {} -h".format(sys.argv[0]))
+
+		try:
+			self.loadFaces()
+			self.loop.run_until_complete(self.startWorkerThreads())
+			time.sleep(3)
+			self.loop.run_until_complete(self.extractVideoFrames())
+		except Exception as e: 	
+			print(e)
+			print('[ OK ] Attempting to stop threads!')
+			for thread in self.threads:
+				thread.terminate()
+				thread.join()
+
+	def loadFaces(self):
+		if os.path.isfile(self.config):
+			print('[ OK ] Loading config...')
+			config = []
+			with open (self.config, 'rb') as file:
+				file = file.read()
+				config = json.loads(file)
+
+			for name in config:
+				print('[ OK ] Loading faces for {}'.format(name))
+				person = config[name]
+				label = person["label"]
+				os.makedirs(os.path.join(self.target_dir, label), exist_ok=True)
+				if os.path.exists(person["faces_dir"]):
+					for _dir, _subdir, files in os.walk(person["faces_dir"]):
+						for file in files:
+							try:
+								file = os.path.join(person["faces_dir"], file)
+								image = face_recognition.load_image_file(file)
+								faces = face_recognition.face_locations(image, model=self.model)
+								encoding = face_recognition.face_encodings(image, faces, num_jitters=self.jitters, model="large")[0]
+
+								self.encodings.append(encoding)
+								self.faces.append({
+									"name": name,
+									"label": label,
+									"group": person["group"]
+								})
+							except Exception as e: print('[ERROR] Failed loading file: {}, error: {}'.format(file, str(e)))
+		else: print("[ERROR] Config file does not exist!")
+
+	async def startWorkerThreads(self):
+		for i in range(self.workers):
+			thread = FaceRipperWorker(
+				queue = self.shared_list,
+				id = (i + 1),
+				tolerance = self.tolerance,
+				encodings = self.encodings,
+				faces = self.faces,
+				target_dir = self.target_dir,
+				model = self.model,
+				blur = self.blur_threshold
+			)
+			
+			thread.start()
+			self.threads.append(thread)
+
+	async def extractVideoFrames(self):
+		for _dir, _subdir, files in os.walk(self.videos):
+			for file in files:
+				file = os.path.normpath(os.path.join(self.videos, file))
+				frames = []
+				frame_count = 0
+
+				print('[ OK ] Extracting faces from video: {}'.format(file))
+				vc = cv2.VideoCapture(file)
+				while vc.isOpened():
+					if len(self.shared_list) < self.workers:
+						ret, frame = vc.read()
+						if not ret: break
+
+						frame = frame[:, :, ::-1]
+						frame_count += 1
+
+						self.shared_list.append(frame)
+			
+			print('[ OK ] Finished...')
+			for thread in self.threads:
+				thread.terminate()
+					
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Extract faces from videos based on predefined settings', add_help=True)
+	parser.add_argument('--config', type=str, default=None, help='Config file to parse in')
+	parser.add_argument('--video-dir', type=str, default=None, help='Directory containing all the videos to extract faces from')
+	parser.add_argument('--target-dir', type=str, default=None, help='Output folder to store the faces in')
+	parser.add_argument('--tolerance', type=float, default=0.35, help='Tolerance for facial recognition, ranging from 0.0 - 1.0 where the lower the number the stricter the matching. The more reference images you have, I recommend lowering it. Default is 0.35 (35%)')
+	parser.add_argument('--threads', type=int, default=4, help='Number of threads to use. Default is 4')
+	parser.add_argument('--jitters', type=int, default=0, help='Number of times to resample face detection for reference images. Higher is more accuracy but slower. e.g, 100 is a 100x more slower.')
+	parser.add_argument('--model', type=str, default='hog', help='(UNSTABLE) Which face detection model to use. "hog" is less accurate but faster on CPUs. "cnn" is a more accurate deep-learning model which is GPU/CUDA accelerated (if available). Default is "hog". This can only be used when using 1 thread')
+	parser.add_argument('--blur', type=float, default=100, help='The blur threshold to use when getting rid of faces that are too blurry. Default is 100.')
+	args = parser.parse_args()
+	
+	clean = FaceRipper(**vars(args))
---
+++ src/worker.py
@@ -0,0 +1,80 @@
+import sys, os, queue, json, face_recognition, cv2, time, numpy as np, string, random
+from multiprocessing import Process
+from PIL import Image
+
+class FaceRipperWorker(Process):
+
+	def __init__(self, queue = None, id = None, tolerance = None, encodings = None, faces = None, target_dir = None, model = None, blur = 100):
+		super(FaceRipperWorker, self).__init__()
+		self.queue = queue
+		self.id = id if id != None else 1
+		self.tolerance = tolerance
+		self.encodings = encodings
+		self.target_dir = target_dir
+		self.faces = faces
+		self.model = model
+		self.blur_threshold = blur
+		self.start_time = time.time()
+		self.compute_times = [ self.start_time - self.start_time + 0.01 ]
+		self.items_pass = 0
+		self.items = 0
+		self.completed_faces = 0
+
+	def run(self):
+		print('[ WORKER #{} ] Starting worker with {}% tolerance and {}/{} known encodings (model = "{}")...'.format(self.id, self.tolerance * 100, len(self.encodings), len(self.faces), self.model))
+		self.startWorking()
+
+	def startWorking(self):
+		while True:
+			if len(self.queue) > 0:
+				try:
+					item = self.queue.pop()
+					self.work(item)
+				except IndexError: pass
+
+	def work(self, item):
+		try:
+			self.start_time = time.time()
+			if self.items_pass % 15 == 0: print('[ WORKER #{} ] Processing item, average time to process per item is {:,.2f}ms. Completed {:,} items so far, and {:,} faces extracted.'.format(self.id, np.average(self.compute_times), self.items_pass, self.completed_faces))
+			faces = face_recognition.face_locations(item, model=self.model)
+			encodings = face_recognition.face_encodings(item, model="large")
+			for i in range(len(encodings)):
+				encoding = encodings[i]
+				face = faces[i]
+				for index in range(len(self.encodings)):
+					enc = self.encodings[index]
+					results = face_recognition.face_distance([ enc ], encoding)
+					if results[0] <= self.tolerance:
+						filename = self.generateFileName(os.path.join(self.target_dir, self.faces[index]["label"]), extension = ".jpg")
+						output_path = os.path.normpath(os.path.join(self.target_dir, self.faces[index]["label"], filename))
+						escaped_path = output_path.replace("\\", "\\\\")
+						top, right, bottom, left = face
+						image = Image.fromarray(item[top:bottom, left:right])
+						width, height = image.size
+						blurry = self.is_blurry(item[top:bottom, left:right])
+						if width > 100 and height > 100 and not blurry:
+							image.save(escaped_path)
+							self.completed_faces += 1
+			self.items += 1
+		except RuntimeError:
+			self.queue.append(item)
+			pass
+		except Exception as e: print('[ WORKER #{} ] Error induced while working on item: {}'.format(self.id, str(e)))
+		self.calculateComputeTime()
+		self.items_pass += 1
+
+	def calculateComputeTime(self):
+		if len(self.compute_times) >= 50: self.compute_times = self.compute_times[0:48]
+		self.compute_times.insert(0, time.time() - self.start_time)
+
+	def randomFileName(self, length=16, extension = ''):
+		chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
+		return ''.join(random.choice(chars) for i in range(length)) + extension
+ 
+	def generateFileName(self, target_dir, length = 16, extension = ''):
+		id = self.randomFileName(length = length, extension = extension)
+		while os.path.exists(os.path.join(target_dir, id)): id = self.randomFileName(extension)
+		return id
+	
+	def is_blurry(self, image):
+		return cv2.Laplacian(image, cv2.CV_64F).var() > self.blur_threshold
